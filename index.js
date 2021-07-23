8b54fe079a1c1ad1ceeb146ffa5c96caba55bb86
sanjatul <sanjatulhasansiam.iit@gmail.com>
2021-07-23 22:35:19 +0600

    String Concatination

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-var number = 20;
-console.log(typeof(number))
+// Arithmetic operator -> +, -, *, / , %, ++, --, ** (exponentiation)
+// Assignment operator -> =, +=, -=, *=, /=, %=
 
-number = toString(20);
-console.log(typeof(number))
+var num1 = parseInt(prompt("Enter first number : "));
+var num2 = parseInt(prompt("Enter second number : "));
+var linebreak = "<br/>";
+var firstName = "Sanjatul";
+var result = num1 + num2;
+document.write("The sum is: " + result + linebreak);
 
-var x = 2.56789;
-// number of characters after the decimal point
-console.log(x.toFixed(1))
-console.log(x.toFixed(2))
-
-
-// total number of characters including the decimal point
-console.log(x.toPrecision(1))
-console.log(x.toPrecision(2))
-
-
-console.log(Number(true))
-console.log(Number(false))
-console.log(Number(" 10"))
-console.log(Number(" 10 "))
-console.log(Number("10.25"))
+document.write("My name is " + firstName + " and I'm 22 years old");
