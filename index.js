bde66de803d71a4a4ebefcad848ddbc99004d776
sanjatul <sanjatulhasansiam.iit@gmail.com>
2021-07-23 22:50:29 +0600

    String Concatination with Library functions

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// Arithmetic operator -> +, -, *, / , %, ++, --, ** (exponentiation)
-// Assignment operator -> =, +=, -=, *=, /=, %=
-
-var num1 = parseInt(prompt("Enter first number : "));
-var num2 = parseInt(prompt("Enter second number : "));
-var linebreak = "<br/>";
-var firstName = "Sanjatul";
-var result = num1 + num2;
-document.write("The sum is: " + result + linebreak);
-
-document.write("My name is " + firstName + " and I'm 22 years old");
+var text = "Bangladesh";
+var text1 = prompt("Enter a phrase : ");
+var linebreak = "<br/>"
+document.write("Number of characters : " + text.length + linebreak);
+document.write("Index 2 : " + text.charAt(2) + linebreak);
+document.write("In upper case : " + text.toUpperCase() + linebreak);
+document.write("Lowe case : : " + text.toLowerCase() + linebreak);
+document.write("Full phrase : " + text.concat(text1) + linebreak);
+document.write("Slice : " + text.slice(0, 5));
