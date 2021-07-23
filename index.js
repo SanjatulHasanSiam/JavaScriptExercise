c39fdb0473c624ed774b0b400678874cc6f837b8
sanjatul <sanjatulhasansiam.iit@gmail.com>
2021-07-23 23:05:25 +0600

    Mini Calculator

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var text = "Bangladesh";
-var text1 = prompt("Enter a phrase : ");
-var linebreak = "<br/>"
-document.write("Number of characters : " + text.length + linebreak);
-document.write("Index 2 : " + text.charAt(2) + linebreak);
-document.write("In upper case : " + text.toUpperCase() + linebreak);
-document.write("Lowe case : : " + text.toLowerCase() + linebreak);
-document.write("Full phrase : " + text.concat(text1) + linebreak);
-document.write("Slice : " + text.slice(0, 5));
+var num1 = parseFloat(prompt("Enter the First number"));
+var num2 = parseFloat(prompt("Enter the Second number"));
+var linebreak = "<br/>";
+document.write("Sum of " + num1 + " and " + num2 + " is : " + (num1 + num2) + linebreak);
+document.write("Substraction of " + num1 + " and " + num2 + " is : " + (num1 - num2) + linebreak);
+document.write("Multiplication of " + num1 + " and " + num2 + " is : " + (num1 * num2) + linebreak);
+document.write("Division of " + num1 + " and " + num2 + " is : " + (num1 / num2) + linebreak);
